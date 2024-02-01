d17d3a550a3a8248973ef763fa9cc23de5a356d9
paulbah <paul4yesus@yahoo.com>
2024-02-01 12:35:21 -0500

    Updated my github

--- gitpractice/src/gitpractice/Gitpractice.java
+++ gitpractice/src/gitpractice/Gitpractice.java
@@ -3,5 +3,6 @@ package gitpractice;
 public class Gitpractice {
 public static void main(String[] args) {
 	System.out.println("This is first git project, I'm learning");
+	System.out.println( "I updated my github");
 }
 }
